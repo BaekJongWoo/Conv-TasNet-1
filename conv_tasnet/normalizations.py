bce081dee4f89b8c53823d70c01bf277b65cbd68
kaparoo <kaparoo2001@gmail.com>
2021-04-03 21:21:27 +0900

    ADD | Add LayerNormInterface
    - Add interface class, LayerNormInterface for the layer normalizations: gLN, cLN, eLN
    - Add (incomplete) exponential layer normalization (i.e, eLN)
    - Change parent class of gLN and cLN for tf.keras.layers.Layer to
      LayerNormInterface

--- conv_tasnet/normalizations.py
+++ conv_tasnet/normalizations.py
@@ -1,19 +1,19 @@
 import tensorflow as tf
 
 
-class GlobalLayerNorm(tf.keras.layers.Layer):
-    """Global Layer Normalization (i.e., gLN)
+class LayerNormInterface(tf.keras.layers.Layer):
+    """Layer Normalization Interface
 
     Attributes:
         H (int): Number of features
-        gamma (tf.Variable): Trainable parameter of shape=(, 1, H)
         beta (tf.Varaible): Trainable paramter of shape=(, 1, H)
+        gamma (tf.Variable): Trainable parameter of shape=(, 1, H)
         eps (float): Small constant for numerical stability
         multiply (keras.layers.Multiply): Elementwise multiplication layer
     """
 
     def __init__(self, H: int, eps: float = 1e-8, **kwargs):
-        super(GlobalLayerNorm, self).__init__(**kwargs)
+        super(LayerNormInterface, self).__init__(**kwargs)
         self.H = H
         _gamma_init = tf.random_normal_initializer()
         self.gamma = tf.Variable(initial_value=_gamma_init(shape=(1, self.H)),
@@ -23,6 +23,36 @@ class GlobalLayerNorm(tf.keras.layers.Layer):
                                 trainable=True)
         self.eps = eps
         self.multiply = tf.keras.layers.Multiply()
+
+    def call(self, input: tf.Tensor) -> tf.Tensor:
+        """
+        Args:
+            inputs (tf.Tensor): Tensor of shape=(, K, H)
+
+        Returns:
+            outputs (tf.Tensor): Tensor of shape=(, K, H)
+        """
+        raise NotImplementedError("`call` function must be implemented!")
+# LayerNormInterface end
+
+
+class GlobalLayerNorm(LayerNormInterface):
+    """Global Layer Normalization (i.e., gLN)
+
+    Description:
+        Layer normalization for `noncausal` system
+        Inherited from LayerNormInterface
+
+    Attributes:
+        H (int): Number of features
+        beta (tf.Varaible): Trainable paramter of shape=(, 1, H)
+        gamma (tf.Variable): Trainable parameter of shape=(, 1, H)
+        eps (float): Small constant for numerical stability
+        multiply (keras.layers.Multiply): Elementwise multiplication layer
+    """
+
+    def __init__(self, H: int, eps: float = 1e-8, **kwargs):
+        super(GlobalLayerNorm, self).__init__(H=H, eps=eps, **kwargs)
 
     def call(self, inputs: tf.Tensor) -> tf.Tensor:
         """
@@ -39,28 +69,23 @@ class GlobalLayerNorm(tf.keras.layers.Layer):
 # GlobalLayerNorm end
 
 
-class CumulativeLayerNorm(tf.keras.layers.Layer):
+class CumulativeLayerNorm(LayerNormInterface):
     """Cumulative Layer Normalization (i.e., cLN)
+
+    Description:
+        Layer normalization for `causal` system
+        Inherited from LayerNormInterface
 
     Attributes:
         H (int): Number of features
-        gamma (tf.Variable): Trainable parameter of shape=(, 1, H)
         beta (tf.Varaible): Trainable paramter of shape=(, 1, H)
+        gamma (tf.Variable): Trainable parameter of shape=(, 1, H)
         eps (float): Small constant for numerical stability
         multiply (keras.layers.Multiply): Elementwise multiplication layer
     """
 
     def __init__(self, H: int, eps: float = 1e-8, **kwargs):
-        super(CumulativeLayerNorm, self).__init__(**kwargs)
-        self.H = H
-        _gamma_init = tf.random_normal_initializer()
-        self.gamma = tf.Variable(initial_value=_gamma_init(shape=(1, self.H)),
-                                 trainable=True)
-        _beta_init = tf.zeros_initializer()
-        self.beta = tf.Variable(initial_value=_beta_init(shape=(1, self.H)),
-                                trainable=True)
-        self.eps = eps
-        self.multiply = tf.keras.layers.Multiply()
+        super(CumulativeLayerNorm, self).__init__(H=H, eps=eps, **kwargs)
 
     def call(self, inputs: tf.Tensor) -> tf.Tensor:
         """
@@ -81,3 +106,36 @@ class CumulativeLayerNorm(tf.keras.layers.Layer):
         outputs = tf.concat(outputs, -2)
         return outputs
 # CumulativeLayerNorm end
+
+
+class ExponentialLayerNorm(LayerNormInterface):
+    """Exponential Layer Normalization (i.e., eLN)
+
+    Description:
+        Layer normalization for `causal` system
+        Inherited from LayerNormInterface
+
+    Attributes:
+        H (int): Number of features
+        alpha (float): Forgetting rate
+        beta (tf.Varaible): Trainable paramter of shape=(, 1, H)
+        gamma (tf.Variable): Trainable parameter of shape=(, 1, H)
+        eps (float): Small constant for numerical stability
+        omega (float): Exponent
+        multiply (keras.layers.Multiply): Elementwise multiplication layer
+    """
+
+    def __init__(self, H, alpha: float = 0.5, eps: float = 1e-8, omega: float = 0.5, **kwargs):
+        super(ExponentialLayerNorm, self).__init__(H=H, eps=eps, **kwargs)
+        self.alpha, self.omega = alpha, omega
+
+    def call(self, inputs: tf.Tensor) -> tf.Tensor:
+        """
+        Args:
+            inputs (tf.Tensor): Tensor of shape=(, K, H)
+
+        Returns:
+            outputs (tf.Tensor): Tensor of shape=(, K, H)
+        """
+        return inputs  # TODO | must fix this line
+# ExponentialLayerNorm end
